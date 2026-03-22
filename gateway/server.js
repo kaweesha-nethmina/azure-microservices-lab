ac38d4d177135b880dacb0775961aad8a194aa53
Kaweesha Nethmina <kaweesha.nj@gmail.com>
2026-03-22 13:25:22 +0530

    test: flow

--- gateway/server.js
+++ gateway/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const https = require("https");
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const API_URL = process.env.API_URL || "http://localhost:5000";
